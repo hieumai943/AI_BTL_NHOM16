0637ed79802986031223084911a589ebfd5002fd
Daohuy2409 <21020329@vnu.edu.vn>
2023-11-12 22:06:06 +0700

    question 3

--- 6_machineLearning/models.py
+++ 6_machineLearning/models.py
@@ -144,6 +144,9 @@ class DigitClassificationModel(object):
         """
         "*** YOUR CODE HERE ***"
 
+        relued = nn.ReLU(nn.AddBias(nn.Linear(x, self.wf), self.bf))
+        return nn.AddBias(nn.Linear(relued, self.wr), self.br)
+    
     def get_loss(self, x, y):
         """
         Computes the loss for a batch of examples.
@@ -159,11 +162,28 @@ class DigitClassificationModel(object):
         """
         "*** YOUR CODE HERE ***"
 
+        return nn.SoftmaxLoss(self.run(x), y)
+    
     def train(self, dataset):
         """
         Trains the model.
         """
         "*** YOUR CODE HERE ***"
+
+        mistakes = 1
+        while mistakes > 0:
+            mistakes = 0
+            for x, y in dataset.iterate_once(self.batch_size):
+                losses = self.get_loss(x, y)
+                gradient = nn.gradients(losses, [self.wf, self.wr, self.bf, self.br])
+                self.wf.update(gradient[0], -0.009)
+                self.wr.update(gradient[1], -0.009)
+                self.bf.update(gradient[2], -0.009)
+                self.br.update(gradient[3], -0.009)
+                
+            val = dataset.get_validation_accuracy()
+            if val < 0.97:
+                mistakes += 1
 
 class LanguageIDModel(object):
     """
